2881ab727dd5332fa32e22068543f47bf6d3cc69
Linse Mathew <linsemathew123@gmail.com>
2016-08-28 23:43:36 -0400

    Rotate arrow.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 var action = "click";
 var speed = "500";
 
-$(document).ready(function{
-	$('.li.q').on(action, function(){
+$(document).ready(function(){
+	$('li.q').on(action, function(){
 		$(this).next()
-			.slideToggle(speed);
+			.slideToggle(speed)
+				.siblings('li.a')
+					.slideUp();
+		var img = $(this).children('img');
+		$('img').not(img).removeClass('rotate');
+		img.toggleClass('rotate')
 	});
 });
