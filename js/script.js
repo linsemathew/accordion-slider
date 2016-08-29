7e7f09ef4ef821e022d688a00ad6b85445815bf7
Linse Mathew <linsemathew123@gmail.com>
2016-08-28 23:32:45 -0400

    Add slide toggle.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-  $( function() {
-    $( "#accordion" ).accordion();
-  } );
+var action = "click";
+var speed = "500";
+
+$(document).ready(function{
+	$('.li.q').on(action, function(){
+		$(this).next()
+			.slideToggle(speed);
+	});
+});
